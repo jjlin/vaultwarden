681257bb6c7bed892de7b17ba0d711b51ecdd4ef
Jeremy Lin <jeremy.lin@gmail.com>
2020-10-09 04:33:40 -0700

    Build cleanup

--- hooks/arches.sh
+++ hooks/arches.sh
@@ -1,7 +1,5 @@
-# The default Debian-based images support these arches for all database connections
-#
-# Other images (Alpine-based) currently
-# support only a subset of these.
+# The default Debian-based images support these arches for all database backends.
+# Alpine-based images currently support only a subset of these.
 arches=(
     amd64
     arm32v6
@@ -10,7 +8,6 @@ arches=(
 )
 
 if [[ "${DOCKER_TAG}" == *alpine ]]; then
-    # The Alpine build currently only works for amd64.
     os_suffix=.alpine
     arches=(
         amd64
